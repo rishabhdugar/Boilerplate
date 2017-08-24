705ace66ee190796102e7849e4d8319c1e705513
Tigran Sarkisian <1731046@gmail.com>
2017-08-24 17:34:17 +0300

    [*] Refactored code.

--- app/src/main/java/com/sarkisian/template/ui/activity/MainActivity.java
+++ app/src/main/java/com/sarkisian/template/ui/activity/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener,
         findViews();
         setListeners();
         customizeActionBar();
-        initDrawer();//87877968
+        initDrawer();
         openScreen(MainFragment.newInstance(), false, R.id.nav_one);
     }
 
